c271ee733592d37b5bb0e7d34520aaec1d998d56
Nick Shrine <nick.shrine@leicester.ac.uk>
2024-02-07 15:55:43 +0000

    renamed step 1

--- install_regenie_step1.sh
+++ install_regenie_step1.sh
@@ -15,7 +15,7 @@ INPUTS=inputs_RAP.json
 PHENO=pheno.txt
 COVAR=covar.txt
 DXCOMPILER=/tmp/dxCompiler.jar
-WDL="rare_variant_analysis.wdl"
+WDL="regenie_step1.wdl"
 
 dx select $PROJECT_ID &&
 dx mkdir -p $PROJECT_DIR &&
@@ -26,7 +26,7 @@ dx ls | grep -w $COVAR && dx rm $COVAR
 dx upload ${BASE}/$PHENO &&
 dx upload ${BASE}/$COVAR &&
 
-./make_RAP_inputs.sh ${PROJECT_DIR}/${PHENO} ${PROJECT_DIR}/${COVAR} $COVARCOLLIST $CATCOVARLIST > $INPUTS &&
+./make_regenie_step1_inputs.sh ${PROJECT_DIR}/${PHENO} ${PROJECT_DIR}/${COVAR} $COVARCOLLIST $CATCOVARLIST > $INPUTS &&
 
 wget https://github.com/dnanexus/dxCompiler/releases/download/2.11.4/dxCompiler-2.11.4.jar -O $DXCOMPILER &&
 
--- proc_collapsing_results.R
+++ proc_collapsing_results.R
@@ -11,4 +11,32 @@ results_fmt <- results %>%
 results_long <- results_fmt %>%
   pivot_longer(ends_with("_zscore")) %>%
   separate(name, c("coef", "PHENO"), sep="\\.") %>%
-  pivot_wider(names_from = coef, values_from = value)
+  pivot_wider(names_from = coef, values_from = value) %>%
+  mutate(P=10^-LOG10P) %>%
+  select(-CHISQ, -LOG10P)
+
+n_tests <- n_distinct(results_long$GENE)
+P_THRESH <- 0.05/n_tests
+
+all_genes <- scan("all_genes.txt", character())
+priority_genes <- scan("priority_genes.txt", character())
+
+top_hits <- results_long %>%
+  filter(TEST == "GENE_P" & P < P_THRESH) %>%
+  group_by(GENE) %>%
+  filter(P == min(P)) %>%
+  ungroup %>%
+  arrange(P) %>%
+  select(PHENO, CHROM, GENE, GENE_P=P) %>%
+  mutate(all_genes = GENE %in% all_genes, priority_genes = GENE %in% priority_genes)
+
+top_hits_specific <- top_hits %>%
+  left_join(results_long, by=c("PHENO", "CHROM", "GENE")) %>%
+  filter(!is.na(THRESH)) %>%
+  arrange(P, THRESH) %>%
+  group_by(GENE) %>%
+  slice(1) %>%
+  ungroup %>%
+  select(-ALLELE0, -ALLELE1, -EXTRA)
+
+write_tsv(top_hits_specific, "LF_collapsing_top_hits.tsv")
